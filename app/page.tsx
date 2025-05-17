528263f72d05f8c2781140b92dc08b80e26a79a5
Gerardo Rodríguez <acidbjazz@gmail.com>
2025-05-17 02:47:58 -0500

    feat: home UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,47 @@
 import styles from "@styles/home.module.sass";
 
+import Image from "next/image";
+import Family from "@assets/family.webp";
+
 export default function Home() {
   return (
     <section className={styles.home}>
-      <div className={styles.content}>Home</div>
+      <Image className={styles.image} src={Family} alt="Family" priority />
+      <form>
+        <h1>Seguro Salud Flexible</h1>
+        <h2>Creado para ti y tu familia</h2>
+        <p className={styles.description}>
+          Tú eliges cuánto pagar. Ingresa tus datos, cotiza y recibe nuestra asesoría. 100% online.
+        </p>
+        <div className={styles.doc}>
+          <label>Nro. de documento</label>
+          <div>
+            <select>
+              <option>DNI</option>
+              <option>C.E.</option>
+            </select>
+            <input type="text" />
+          </div>
+        </div>
+        <div className={styles.cell}>
+          <label>Celular</label>
+          <input type="text" />
+        </div>
+        <div className={styles.check}>
+          <label>
+            <input type="checkbox" />
+            <span>Acepto lo Política de Privacidad</span>
+          </label>
+        </div>
+        <div className={styles.check}>
+          <label>
+            <input type="checkbox" />
+            <span>Acepto la Política Comunicaciones Comerciales</span>
+          </label>
+        </div>
+        <a href="#">Aplican Términos y Condiciones.</a>
+        <button type="submit">Cotiza aquí</button>
+      </form>
     </section>
   );
 }
